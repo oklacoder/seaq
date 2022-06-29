f7d378a3a8f0a68c662afd6ab5515aa1024ae2dc
Matt Stovall <stovall88@hotmail.com>
2022-06-29 08:03:27 -0500

    class/file cleanup

---
+++ src/seaq/Aggregations/DateHistogramAggregationRequest.cs
@@ -0,0 +1,38 @@
+﻿using Nest;
+
+namespace seaq
+{
+    public class DateHistogramAggregationRequest :
+        DefaultAggregationRequest
+    {
+        private readonly string interval;
+        private readonly string offset;
+        private readonly int? minBucketSize;
+
+        public DateHistogramAggregationRequest(
+            DefaultAggregationField field,
+            string interval = null,
+            string offset = null,
+            int? minBucketSize = null)
+            : base(DefaultAggregationCache.DateHistogramAggregation.Name, field)
+        {
+            this.interval = interval;
+            this.offset = offset;
+            this.minBucketSize = minBucketSize;
+        }
+
+        public override AggregationContainerDescriptor<T> ApplyAggregationDescriptor<T>(
+            AggregationContainerDescriptor<T> agg,
+            IAggregationCache aggregationCache)
+        {
+
+            return new DateHistogramAggregation(interval, offset, minBucketSize).ApplyAggregationDescriptor<T>(agg, Field);
+        }
+        public override AggregationContainerDescriptor<T> GetAggregationDescriptor<T>(
+            IAggregationCache aggregationCache)
+        {
+            return new DateHistogramAggregation(interval, offset, minBucketSize).GetAggregationDescriptor<T>(Field);
+        }
+    }
+
+}
---
+++ src/seaq/Aggregations/HistogramAggregationRequest.cs
@@ -0,0 +1,38 @@
+﻿using Nest;
+
+namespace seaq
+{
+    public class HistogramAggregationRequest :
+        DefaultAggregationRequest
+    {
+        private readonly double? interval;
+        private readonly double? offset;
+        private readonly int? minBucketSize;
+
+        public HistogramAggregationRequest(
+            DefaultAggregationField field,
+            double? interval = null,
+            double? offset = null,
+            int? minBucketSize = null)
+            : base(DefaultAggregationCache.DateHistogramAggregation.Name, field)
+        {
+            this.interval = interval;
+            this.offset = offset;
+            this.minBucketSize = minBucketSize;
+        }
+
+        public override AggregationContainerDescriptor<T> ApplyAggregationDescriptor<T>(
+            AggregationContainerDescriptor<T> agg,
+            IAggregationCache aggregationCache)
+        {
+
+            return new HistogramAggregation(interval, offset, minBucketSize).ApplyAggregationDescriptor<T>(agg, Field);
+        }
+        public override AggregationContainerDescriptor<T> GetAggregationDescriptor<T>(
+            IAggregationCache aggregationCache)
+        {
+            return new HistogramAggregation(interval, offset, minBucketSize).GetAggregationDescriptor<T>(Field);
+        }
+    }
+
+}
