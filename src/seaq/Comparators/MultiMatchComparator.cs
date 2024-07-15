f55654e09f9ea529dee2d5f8d8caa354a7e342f4
Matt Stovall <stovall88@hotmail.com>
2024-07-15 15:28:13 -0500

    clean up 1:1 file:class

--- src/seaq/Comparators/MultiMatchComparator.cs
+++ src/seaq/Comparators/MultiMatchComparator.cs
@@ -12,16 +12,4 @@
 
         }
     }
-
-    public class ORComparator : 
-        BaseComparator
-    {
-        const string _display = "OR";
-        const string _value = "or";
-
-        public ORComparator()
-            : base(_display, _value)
-        {                
-        }
-    }
 }
---
+++ src/seaq/Comparators/ORComparator.cs
@@ -0,0 +1,14 @@
+﻿namespace seaq
+{
+    public class ORComparator : 
+        BaseComparator
+    {
+        const string _display = "OR";
+        const string _value = "or";
+
+        public ORComparator()
+            : base(_display, _value)
+        {                
+        }
+    }
+}
